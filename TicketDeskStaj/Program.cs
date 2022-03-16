f1550e7493955a173cc81a4881fced07cda6b480
esyolal <esyolal@gmail.com>
2022-03-16 13:11:23 +0300

    deneme

--- TicketDeskStaj/Program.cs
+++ TicketDeskStaj/Program.cs
@@ -16,6 +16,7 @@ namespace TicketDeskStaj
             CreateHostBuilder(args).Build().Run();
         }
 
+
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
